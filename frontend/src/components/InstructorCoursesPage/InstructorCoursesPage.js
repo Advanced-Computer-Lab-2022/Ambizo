421db06fb7df3d524bc748e9514bc337ec3c8257
omartarek126 <omarelrasas12@gmail.com>
2022-11-26 03:48:11 +0200

    Added interceptors to update axios headers with token

--- frontend/src/components/InstructorCoursesPage/InstructorCoursesPage.js
+++ frontend/src/components/InstructorCoursesPage/InstructorCoursesPage.js
@@ -192,7 +192,6 @@ function InstructorCoursesPage() {
             </>
         )
     }
-    
     return (
         <>
             {isLoading ? 
--- frontend/src/http-common-post.js
+++ frontend/src/http-common-post.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
-export default axios.create({
+const apiClient = axios.create({
     baseURL: "http://localhost:5000/api",
-    headers: {
-        "Authorization": sessionStorage.getItem("Token")
-    }
+    headers: {}
 });
+
+apiClient.interceptors.request.use(function (config) {
+    config.headers["Authorization"] = sessionStorage.getItem("Token");
+    return config;
+});
+
+export default apiClient;
