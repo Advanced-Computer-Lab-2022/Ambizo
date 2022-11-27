108fb066f1f3ade7c41234766e178a24bcfde8de
Abdelrahman Hanafy <AbdelrahmanHanafyDev@gmail.com>
2022-11-27 02:22:34 +0200

    The backend of the forget password

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ import corporateTrainee from "./routes/corporateTrainee.route.js";
 import guest from "./routes/guest.route.js";
 import instructor from "./routes/instructor.route.js";
 import administrator from "./routes/administrator.route.js";
+import password from "./routes/password.route.js"
 import user from "./routes/user.route.js";
+
 
 const app = express();
 
@@ -17,7 +19,9 @@ app.use("/api/user", user);
 app.use("/api/admin", administrator);
 app.use("/api/instructor", instructor);
 app.use("/api/corporateTrainee", corporateTrainee);
+app.use("/api/password", password);
 app.use("*", (req, res) => res.status(404).send("Error: Not Found"));
+
 
 
 export default app;
