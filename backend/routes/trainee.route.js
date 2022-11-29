06708806cb0f907b4a70de64d14a24dfd8eb5a0d
Abdelrahman Hanafy <AbdelrahmanHanafyDev@gmail.com>
2022-11-29 19:05:59 +0200

    Added backend of rating instructor.

--- backend/routes/trainee.route.js
+++ backend/routes/trainee.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import verifyJWT from "../middleware/verifyJWT.js";
 import course from "../models/course.model.js";
+import instructor from "../models/instructor.model.js";
 import corporateTrainee from "../models/corporateTrainee.model.js";
 import individualTrainee from "../models/individualTrainee.model.js";
 import user from "../models/user.model.js";
@@ -164,10 +165,19 @@ router.post('/rateCourse/:courseId', verifyJWT, async (req, res) => {
 
 router.put('/updateCourseRating/:courseId', verifyJWT, async (req, res) => {
 
-    const { Rating, Review } = req.body;
+    let Rating;
+    let Review;
+
+    if(req.body.Rating){
+        Rating = req.body.Rating;
+    }
+
+    if(req.body.Review){
+        Review = req.body.Review;
+    }
 
     if( !Rating && !Review){
-        return req.status(400).json({message: 'At least the (Rating) or the (Review) fields must be provided in the body.'}); 
+        return res.status(400).json({message: 'At least the (Rating) or the (Review) fields must be provided in the body.'}); 
     }
 
     const { Username, Type } = req.User;
@@ -222,7 +232,7 @@ router.put('/updateCourseRating/:courseId', verifyJWT, async (req, res) => {
             const oldRating = listOfRatings[rating].Rating;
             const oldReview = listOfRatings[rating].Review;
 
-            listOfRatings[rating].Rating = Rating;
+            listOfRatings[rating].Rating = Rating? Rating: oldRating;
 
             course.updateOne(
                 {_id:courseId, "Ratings.TraineeUsername": Username},
@@ -302,11 +312,15 @@ router.delete('/deleteCourseRating/:courseId', verifyJWT, async (req, res) => {
     for(var rating in courseToRate.Ratings){
         if(courseToRate.Ratings[rating].TraineeUsername === Username){
             isRatingFound = true;
+            
             course.updateOne(
                 {_id : courseId},
                 {
                     $pull:{
                         Ratings: {TraineeUsername: Username}
+                    },
+                    $set:{
+                        Rating: calculateNewRating(courseToRate.Ratings.filter(rating => rating.TraineeUsername !== Username))
                     }
                 }
             ).then(_ => {
@@ -320,7 +334,169 @@ router.delete('/deleteCourseRating/:courseId', verifyJWT, async (req, res) => {
     if(!isRatingFound){
         return res.status(409).json({message: 'The user did not rate this course.'})
     }
- });
+});
+
+router.post('/rateInstructor/:instructorUsername', verifyJWT, async (req, res) => {
+
+    if( !req.body.Rating || !req.body.Review){
+        return res.status(400).json({message: 'The (Rating) and the (Review) fields must be provided in the body.'});
+    }
+    const { Rating, Review } = req.body;
+
+    if( !req.User || !req.User.Username || !req.User.Type){
+        return res.status(401).json({message: 'Failed to authenticate the user.'});
+    }
+    const { Username, Type } = req.User;
+    
+    const instructorUsername = req.params.instructorUsername;
+    const requestingUser = await user.findOne({Username: Username});
+
+    //making sure the request come from valid user with valid type
+    if ((requestingUser.Type !== Type) || (Type !== 'corporateTrainee' && Type !== 'individualTrainee')){
+        return res.status(403).json({message: 'The user is not allowed to rate instructors.'})
+    }
+
+    const instructorToRate =await instructor.findOne({Username: instructorUsername});
+    if(!instructorToRate){
+        return res.status(404).json({message: 'There is no instructor with this username.'})
+    }
+
+    for(var rating in instructorToRate.Ratings){
+        if(instructorToRate.Ratings[rating].TraineeUsername === Username){
+            return res.status(409).json({message: 'The user has already rated this instructor.'})
+        }
+    }
+
+    const newInstructorRating = {
+        TraineeUsername: Username,
+        Rating: Rating,
+        Review: Review
+    };
+    instructorToRate.Ratings.push(newInstructorRating);
+    instructorToRate.Rating = calculateNewRating(instructorToRate.Ratings);
+
+    instructorToRate.save().then(_ => {
+        return res.status(201).json({message: 'The rating has been added successfully.'})
+    }).catch(error => { 
+        console.log(error);
+        return res.status(500).json({message: 'An error has occured while adding the new rating.'})
+    });
+
+});
+
+router.put('/updateInstructorRating/:instructorUsername', verifyJWT, async (req, res) => {
+    let Rating;
+    let Review;
+
+    if(req.body.Rating){
+        Rating = req.body.Rating;
+    }
+
+    if(req.body.Review){
+        Review = req.body.Review;
+    }
+
+    if( !Rating && !Review){
+        return res.status(400).json({message: 'At least the (Rating) or the (Review) fields must be provided in the body.'}); 
+    }
+
+    if( !req.User || !req.User.Username || !req.User.Type){
+        return res.status(401).json({message: 'An error has occurred while authenticating the user.'})
+    }
+    const { Username, Type } = req.User;
+    const instructorUsername = req.params.instructorUsername;
+    const requestingUser = await user.findOne({Username: Username});
+
+    //making sure the request come from valid user with valid type
+    if ((requestingUser.Type !== Type) || (Type !== 'corporateTrainee' && Type !== 'individualTrainee')){
+        return res.status(403).json({message: 'The user is not allowed to rate instructors.'})
+    }
+
+    const instructorToRate = await instructor.findOne({Username: instructorUsername});
+    if(!instructorToRate){
+        return res.status(404).json({message: 'There is no instructor with this username.'})
+    }
+
+    let instructorRated = false;
+
+    for(var rating in instructorToRate.Ratings){
+        if(instructorToRate.Ratings[rating].TraineeUsername === Username){
+            const oldRating = instructorToRate.Ratings[rating].Rating;
+            const oldReview = instructorToRate.Ratings[rating].Review;
+            instructorToRate.Ratings[rating].Rating = Rating? Rating: oldRating; 
+            instructorRated = true;
+            instructor.updateOne(
+                {Username: instructorUsername, "Ratings.TraineeUsername": Username },
+                {
+                    $set: {
+                        Rating: calculateNewRating(instructorToRate.Ratings),
+                        'Ratings.$.Rating': Rating? Rating : oldRating,
+                        'Ratings.$.Review': Review? Review : oldReview
+                    }
+                }
+            ).then(_ => {
+                return res.status(201).json({message: 'The rating has been updated successfully.'})
+            }).catch(error => {
+                //console.log(error);
+                return res.status(500).json({message: 'An error has occurred while updating the rating.'})
+            });
+        }
+        break;
+    }   
+
+    if( !instructorRated ){
+        return res.status(409).json({message: 'The user did not rate this instructor before.'});
+    }
+    
+
+})
+
+router.delete('/deleteInstructorRating/:instructorUsername', verifyJWT, async (req, res) => {
+    if( !req.User || !req.User.Username || !req.User.Type){
+        return res.status(401).json({message: 'An error has occurred while authenticating the user.'})
+    }
+    const { Username, Type } = req.User;
+    const instructorUsername = req.params.instructorUsername;
+    const requestingUser = await user.findOne({Username: Username});
+
+    //making sure the request come from valid user with valid type
+    if ((requestingUser.Type !== Type) || (Type !== 'corporateTrainee' && Type !== 'individualTrainee')){
+        return res.status(403).json({message: 'The user is not allowed to rate instructors.'})
+    }
+
+    const instructorToRate = await instructor.findOne({Username: instructorUsername});
+    if(!instructorToRate){
+        return res.status(404).json({message: 'There is no instructor with this username.'})
+    }
+
+    let instructorRated = false;
+    for(var rating in instructorToRate.Ratings){
+        if(instructorToRate.Ratings[rating].TraineeUsername === Username){
+            instructorRated = true;
+            instructor.updateOne(
+                {Username: instructorUsername},
+                {
+                    $pull: {
+                        Ratings: {TraineeUsername: Username}
+                    },
+                    $set: {
+                        Rating: calculateNewRating(instructorToRate.Ratings.filter(rating => rating.TraineeUsername !== Username)),
+                    }
+                }
+            ).then(_ => {
+                return res.status(201).json({message: 'The rating has been removed successfully.'})
+            }).catch(error => {
+                console.log(error);
+                return res.status(500).json({message: 'An error has occurred while removing the rating.'})
+            });
+            break;
+        }
+    }   
+
+    if( !instructorRated ){
+        return res.status(409).json({message: 'The user did not rate this instructor before.'});
+    }
+});
 
 function handleError(res, err) {
     return res.status(400).send(err);
