aad0510400787f8bf3fcb68a09755829901f088e
omartarek126 <omarelrasas12@gmail.com>
2022-12-04 23:48:06 +0200

    404 message edit

--- frontend/src/components/404/NotFound.js
+++ frontend/src/components/404/NotFound.js
@@ -30,7 +30,7 @@ function NotFound() {
                     <Header />
                     <div>
                         <img src={errorImage} alt="404 - Not Found" className='error404Image' />
-                        <h1 className='error404Text'>The Page You Were Looking For Doesn't Exist.</h1>
+                        <h1 className='error404Text'>The Page You Are Looking For Doesn't Exist.</h1>
                         <h3 className='error404Text'>Or you may not have access, if you have any questions, please contact us at: <a href = "mailto: support@ambizo.com">support@ambizo.com</a></h3>
                     </div>
                 </>
